fb29babbc5cd55ab6385b799b0f5ce6c21a50bff
Castiglione <nicole.castiglione@accusys.com.ar>
2024-09-09 12:52:25 -0300

    changed config jest

--- babel.config.js
+++ babel.config.js
@@ -1,41 +1,27 @@
 
-/*
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset' ],
-    env: {
-      test: {
-        plugins: ['@babel/plugin-transform-modules-commonjs']
-      },
-    }
-  };
-}
-*/
 
-// jest.config.js
-module.exports = {
-  preset: 'react-native',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  setupFiles: ['<rootDir>/jest/setup.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testEnvironment: 'jsdom',
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|react-navigation|@react-navigation|@react-native-community|@react-native-firebase|@react-native-async-storage/async-storage|@react-native-picker|react-native-gesture-handler/.*|react-native-reanimated/.*|@react-native-masked-text/.*|@react-native-svg/.*|@react-native-elements/.*|@react-native-community/.*)'
-  ],
-};
-
-
-
-// module.exports = {
-//   presets: ['babel-preset-expo'],
-//   env: {
-//     test: {
-//       plugins: ['@babel/plugin-transform-modules-commonjs']
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset' ],
+//     env: {
+//       test: {
+//         plugins: ['@babel/plugin-transform-modules-commonjs']
+//       },
 //     }
-//   }
-// 
+//   };
+// }
+
+
+
+
+
+
+module.exports = {
+  presets: ['babel-preset-expo'],
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    }
+  }
+}
---
+++ jest.setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-native/extend-expect';
+
+jest.mock('@react-native-async-storage/async-storage');
+
+
+// jest.mock('@react-native-async-storage/async-storage', () => require('./mocks/@react-native-async-storage/async-storage'));
